910636b39a43ec2f2ad261fddab6af99c9bdc4b6
Jackson <jjsteph@byu.edu>
2025-06-29 14:35:36 -0600

    Began work on ChessMovesCalculator including the switch statement for getting piece type and added necessary imports

--- shared/src/main/java/chess/ChessMovesCalculator.java
+++ shared/src/main/java/chess/ChessMovesCalculator.java
@@ -1,4 +1,21 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class ChessMovesCalculator {
+
+    public Collection<ChessMove> determineMoves(ChessBoard board, ChessPosition myPosition) {
+        return switch (board.getPiece(myPosition).getPieceType()) {
+            case KING -> kingMoves(board, myPosition);
+            case QUEEN -> queenMove(board, myPosition);
+            case BISHOP -> bishopMoves(board, myPosition);
+            case KNIGHT -> knightMoves(board, myPosition);
+            case ROOK -> rookMoves(board, myPosition);
+            case PAWN -> pawnMoves(board, myPosition);
+        };
+    }
+
+
 }
