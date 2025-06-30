a80e98d418d286d8dcad46c0e38f689f7670161a
Jackson <jjsteph@byu.edu>
2025-06-29 21:22:37 -0600

    Added nonworking functions for movement of each piece

--- shared/src/main/java/chess/ChessMovesCalculator.java
+++ shared/src/main/java/chess/ChessMovesCalculator.java
@@ -2,19 +2,42 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class ChessMovesCalculator {
 
     public Collection<ChessMove> determineMoves(ChessBoard board, ChessPosition myPosition) {
         return switch (board.getPiece(myPosition).getPieceType()) {
-            case KING -> kingMoves(board, myPosition);
-            case QUEEN -> queenMove(board, myPosition);
             case BISHOP -> bishopMoves(board, myPosition);
+            case KING -> kingMoves(board, myPosition);
             case KNIGHT -> knightMoves(board, myPosition);
-            case ROOK -> rookMoves(board, myPosition);
             case PAWN -> pawnMoves(board, myPosition);
+            case QUEEN -> queenMoves(board, myPosition);
+            case ROOK -> rookMoves(board, myPosition);
         };
+    }
+
+    private Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition){
+        Collection<ChessMove> move = new ArrayList<>();
+    }
+
+    private Collection<ChessMove> kingMoves(ChessBoard board, ChessPosition myPosition){
+        Collection<ChessMove> move = new ArrayList<>();
+    }
+
+    private Collection<ChessMove> knightMoves(ChessBoard board, ChessPosition myPosition){
+        Collection<ChessMove> move = new ArrayList<>();
+    }
+
+    private Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition){
+        Collection<ChessMove> move = new ArrayList<>();
+    }
+
+    private Collection<ChessMove> queenMoves(ChessBoard board, ChessPosition myPosition){
+        Collection<ChessMove> move = new ArrayList<>();
+    }
+
+    private Collection<ChessMove> rookMoves(ChessBoard board, ChessPosition myPosition){
+        Collection<ChessMove> move = new ArrayList<>();
     }
 
 
