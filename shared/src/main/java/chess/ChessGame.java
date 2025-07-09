4c8b9b24b3976fc9ee58423074d074acd5081422
Jackson <jjsteph@byu.edu>
2025-07-09 14:18:47 -0600

    Implemented validMoves function in ChessGame

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -68,7 +68,11 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-
+        ChessPiece piece = board.getPiece(startPosition);
+        if (piece == null) {
+            return null;
+        }
+        return piece.legalMoves(board, startPosition);
     }
 
     /**
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -75,7 +75,7 @@ public class ChessPiece {
         Collection<ChessMove> legalMoves = new ArrayList<>();
         Collection<ChessMove> possibleMoves = pieceMoves(board, myPosition);
         for (ChessMove possibleMove : possibleMoves) {
-            ChessBoard tempBoard = board.copy();
+            ChessBoard tempBoard = board.copyBoard();
             tempBoard.makeMove(possibleMove);
             if (!tempBoard.isInCheck(this.getTeamColor())){
                 legalMoves.add(possibleMove);
