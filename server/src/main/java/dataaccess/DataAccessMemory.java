a88fbe9d57c6e6bc51bcdeff27b02adec64c2f55
Jackson <jjsteph@byu.edu>
2025-07-16 18:55:16 -0600

    continuing implementation of DataAccessMemory class

--- server/src/main/java/dataaccess/DataAccessMemory.java
+++ server/src/main/java/dataaccess/DataAccessMemory.java
@@ -11,4 +11,41 @@ public class DataAccessMemory implements DataAccess {
     private final Map<String, UserData> users = new HashMap<>();
     private final Map<Integer, GameData> games = new HashMap<>();
     private final Map<String, AuthData> authTokens = new HashMap<>();
+    private int nextGameID = 1;
+
+    @Override
+    public void clearUsers() throws DataAccessException {
+        users.clear();
+    }
+
+    @Override
+    public void clearGames() throws DataAccessException {
+        games.clear();
+        nextGameID = 1;
+    }
+
+    @Override
+    public void clearAuth() throws DataAccessException {
+        authTokens.clear();
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return users.get(username);
+    }
+
+    @Override
+    public void createGame(GameData game) throws DataAccessException {
+        games.put(game.gameID(), game);
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return games.get(gameID);
+    }
 }
