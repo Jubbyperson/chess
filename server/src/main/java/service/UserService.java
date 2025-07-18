d5518514b83f6f7fd19396feee6e22538b48a8b1
Jackson <jjsteph@byu.edu>
2025-07-17 20:50:56 -0600

    Working on UserService.java

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,31 @@
 package service;
 
+import dataaccess.DataAccess;
+import model.AuthData;
+import model.UserData;
+import service.results.*;
+import service.requests.*;
+
+import java.util.UUID;
+
 public class UserService {
+    private final DataAccess dataAccess;
+
+    public UserService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public RegisterResult register(RegisterRequest request) throws Exception {
+        if (dataAccess.getUser(request.username()) != null) {
+            throw new Exception("already taken");
+        }
+
+        UserData user = new UserData(request.username(), request.password(), request.email());
+        dataAccess.createUser(user);
+
+        String authToken = UUID.randomUUID().toString();
+        AuthData auth =  new AuthData(authToken, request.username());
+        dataAccess.createAuth(auth);
+        return new RegisterResult(request.username(), authToken);
+    }
 }
