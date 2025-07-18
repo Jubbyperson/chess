7f886d34feb433685ae1930cca03927d20c4d7eb
Jackson <jjsteph@byu.edu>
2025-07-17 20:58:54 -0600

    ClearService completed and implemented

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,4 +1,20 @@
 package service;
 
+import dataaccess.DataAccess;
+import service.results.ClearResult;
+import service.requests.ClearRequest;
+
 public class ClearService {
+    private final DataAccess dataAccess;
+
+    public ClearService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public ClearResult clear(ClearRequest request) throws Exception{
+        dataAccess.clearUsers();
+        dataAccess.clearAuth();
+        dataAccess.clearGames();
+        return new ClearResult();
+    }
 }
