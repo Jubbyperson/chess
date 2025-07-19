12941678610aeb114ec08587c9037ac21de8a963
Jackson <jjsteph@byu.edu>
2025-07-19 13:08:14 -0600

    Working through handler class

--- server/src/main/java/handler/Handler.java
+++ server/src/main/java/handler/Handler.java
@@ -5,8 +5,8 @@ import service.ClearService;
 import service.GameService;
 import service.UserService;
 import com.google.gson.Gson;
-import service.requests.RegisterRequest;
-import service.results.RegisterResult;
+import service.requests.*;
+import service.results.*;
 import spark.Request;
 import spark.Response;
 
@@ -29,6 +29,54 @@ public class Handler {
             RegisterResult result = userService.register(registerRequest);
             response.status(200);
             return gson.toJson(result);
+        } catch (Exception e){
+            if (e.getMessage().equals("already taken")){
+                response.status(403);
+            } else {
+                response.status(400);
+            }
+            return gson.toJson(new ErrorResponse("Error: " + e.getMessage()))
         }
     }
+
+    public Object login(Request req, Response res) {
+        try {
+            LoginRequest request = gson.fromJson(req.body(), LoginRequest.class);
+            LoginResult result = userService.login(request);
+            res.status(200);
+            return gson.toJson(result);
+        } catch (Exception e) {
+            res.status(401);
+            return gson.toJson(new ErrorResponse("Error: " + e.getMessage()));
+        }
+    }
+
+    public Object logout(Request req, Response res) {
+        try {
+            String authToken = req.headers("authorization");
+            LogoutRequest request = new LogoutRequest(authToken);
+            LogoutResult result = userService.logout(request);
+            res.status(200);
+            return "{}";
+        } catch (Exception e) {
+            res.status(401);
+            return gson.toJson(new ErrorResponse("Error: " + e.getMessage()));
+        }
+    }
+
+    public Object listGames(Request req, Response res) {
+        try {
+            String authToken = req.headers("authorization");
+            ListGamesRequest request = new ListGamesRequest(authToken);
+            ListGamesResult result = gameService.listGames(request);
+            res.status(200);
+            return gson.toJson(result);
+        } catch (Exception e) {
+            res.status(401);
+            return gson.toJson(new ErrorResponse("Error: " + e.getMessage()));
+        }
+    }
+
+
+    private record ErrorResponse(String message){}
 }
