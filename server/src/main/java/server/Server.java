8ae4efde1c45b27ec00ac788aa046ba18b12d4bb
Jackson <jjsteph@byu.edu>
2025-07-19 13:18:30 -0600

    Finished server.java class file

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,16 @@
 package server;
 
+import dataaccess.DataAccess;
+import dataaccess.DataAccessMemory;
+import handler.Handler;
 import spark.*;
 
 public class Server {
+    private final Handler handler;
+
+    public Server(){
+        this.handler = new Handler(new DataAccessMemory());
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,9 +18,18 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
+        Spark.delete("/db", handler::clear);
+        Spark.post("/user", handler::register);
+        Spark.post("/session", handler::login);
+        Spark.delete("/session", handler::logout);
+        Spark.get("/game", handler::listGames);
+        Spark.post("/game", handler::createGame);
+        Spark.put("/game", handler::joinGame);
         //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        Spark.exception(Exception.class, (e, req, res) -> {
+            res.status(500);
+            res.body("{\"message\":\"Error: " + e.getMessage() + "\"}");
+        });
 
         Spark.awaitInitialization();
         return Spark.port();
