25654a1876daf1aa1e20d5fd03e8e929c23858a7
Jackson <jjsteph@byu.edu>
2025-07-23 18:32:26 -0600

    Working on SqlDataAccess file

--- server/src/main/java/dataaccess/SqlDataAccess.java
+++ server/src/main/java/dataaccess/SqlDataAccess.java
@@ -1,7 +1,9 @@
 package dataaccess;
 
+import chess.ChessGame;
 import com.google.gson.*;
 import java.sql.*;
+import model.*;
 
 
 public class SqlDataAccess implements DataAccess{
@@ -100,5 +102,85 @@ public class SqlDataAccess implements DataAccess{
         } catch (SQLException ex) {
             throw new DataAccessException("failed to clear auth", ex);
         }
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        // Check if user already exists
+        if (getUser(user.username()) != null) {
+            throw new DataAccessException("User already exists");
+        }
+
+        var statement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.setString(1, user.username());
+            preparedStatement.setString(2, user.password());
+            preparedStatement.setString(3, user.email());
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to create user", ex);
+        }
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        var statement = "SELECT username, password, email FROM users WHERE username = ?";
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.setString(1, username);
+            try (var rs = preparedStatement.executeQuery()) {
+                if (rs.next()) {
+                    return new UserData(
+                            rs.getString("username"),
+                            rs.getString("password"),
+                            rs.getString("email")
+                    );
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to get user", ex);
+        }
+        return null;
+    }
+
+    @Override
+    public void createGame(GameData game) throws DataAccessException {
+        var statement = "INSERT INTO games (gameID, whiteUsername, blackUsername, gameName, game) VALUES (?, ?, ?, ?, ?)";
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.setInt(1, game.gameID());
+            preparedStatement.setString(2, game.whiteUser());
+            preparedStatement.setString(3, game.blackUser());
+            preparedStatement.setString(4, game.gameName());
+            preparedStatement.setString(5, gson.toJson(game.game()));
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to create game", ex);
+        }
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        var statement = "SELECT gameID, whiteUsername, blackUsername, gameName, game FROM games WHERE gameID = ?";
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.setInt(1, gameID);
+            try (var rs = preparedStatement.executeQuery()) {
+                if (rs.next()) {
+                    ChessGame chessGame = gson.fromJson(rs.getString("game"), ChessGame.class);
+                    return new GameData(
+                            rs.getInt("gameID"),
+                            rs.getString("whiteUsername"),
+                            rs.getString("blackUsername"),
+                            rs.getString("gameName"),
+                            chessGame
+                    );
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to get game", ex);
+        }
+        return null;
     }
 }
