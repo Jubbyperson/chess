557996c2a5701061f7fe1e166cee176038c72e13
Jackson <jjsteph@byu.edu>
2025-07-23 18:29:47 -0600

    Working on SqlDataAccess file

--- server/src/main/java/dataaccess/SqlDataAccess.java
+++ server/src/main/java/dataaccess/SqlDataAccess.java
@@ -1,6 +1,7 @@
 package dataaccess;
 
 import com.google.gson.*;
+import java.sql.*;
 
 
 public class SqlDataAccess implements DataAccess{
@@ -17,5 +18,87 @@ public class SqlDataAccess implements DataAccess{
         createUserTable();
         createAuthTable();
         createGameTable();
+    }
+
+    private void createUserTable() throws DataAccessException {
+        var createUserTable = """
+            CREATE TABLE IF NOT EXISTS users (
+                username VARCHAR(255) NOT NULL PRIMARY KEY,
+                password VARCHAR(255) NOT NULL,
+                email VARCHAR(255)
+            )
+            """;
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(createUserTable)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to create user table", ex);
+        }
+    }
+
+    private void createGameTable() throws DataAccessException {
+        var createGameTable = """
+            CREATE TABLE IF NOT EXISTS games (
+                gameID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                whiteUsername VARCHAR(255),
+                blackUsername VARCHAR(255),
+                gameName VARCHAR(255) NOT NULL,
+                game TEXT NOT NULL
+            )
+            """;
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(createGameTable)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to create game table", ex);
+        }
+    }
+
+    private void createAuthTable() throws DataAccessException {
+        var createAuthTable = """
+            CREATE TABLE IF NOT EXISTS auth (
+                authToken VARCHAR(255) NOT NULL PRIMARY KEY,
+                username VARCHAR(255) NOT NULL
+            )
+            """;
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(createAuthTable)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to create auth table", ex);
+        }
+    }
+
+    @Override
+    public void clearUsers() throws DataAccessException {
+        var statement = "DELETE FROM users";
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to clear users", ex);
+        }
+    }
+
+    @Override
+    public void clearGames() throws DataAccessException {
+        var statement = "DELETE FROM games";
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to clear games", ex);
+        }
+    }
+
+    @Override
+    public void clearAuth() throws DataAccessException {
+        var statement = "DELETE FROM auth";
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to clear auth", ex);
+        }
     }
 }
