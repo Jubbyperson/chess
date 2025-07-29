25ef0e7434bf1d055220043482dff90a9550468b
Jackson <jjsteph@byu.edu>
2025-07-28 22:01:46 -0600

    Working on ServerFacade.java

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,6 +1,8 @@
 package client;
 
 import com.google.gson.Gson;
+import model.*;
+import java.util.List;
 
 public class ServerFacade {
     private final String serverUrl;
@@ -10,4 +12,16 @@ public class ServerFacade {
         this.serverUrl = "http://localhost:" + port;
         this.gson = new Gson();
     }
+
+    private record RegisterRequest(String username, String password, String email) {}
+    private record LoginRequest(String username, String password) {}
+    private record CreateGameRequest(String gameName) {}
+    private record JoinGameRequest(String playerColor, int gameID) {}
+    private record RegisterResult(String username, String authToken) {}
+    private record LoginResult(String username, String authToken) {}
+    private record LogoutResult() {}
+    private record ListGamesResult(List<GameEntry> games) {}
+    private record CreateGameResult(int gameID) {}
+    private record JoinGameResult() {}
+    private record ErrorResponse(String message) {}
 }
