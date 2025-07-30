4d7228e15d66a4cffdd171d096d46dda9f93a64e
Jackson <jjsteph@byu.edu>
2025-07-29 20:57:22 -0600

    main.java file updated to use client class

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,8 @@
-import chess.*;
+import client.Client;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        Client client = new Client(8080); // Default port
+        client.run();
     }
 }
